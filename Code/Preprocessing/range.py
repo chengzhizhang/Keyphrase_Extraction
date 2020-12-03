d4257ae6af27c2e6670278af7c008d351b50a83b
chengzhizhang <61543993+chengzhizhang@users.noreply.github.com>
2020-12-03 20:55:19 +0800

    Update range.py

--- Code/Preprocessing/range.py
+++ Code/Preprocessing/range.py
@@ -1,9 +1,11 @@
+#Reordering the data in the dataset
+
 # -*- coding: utf-8 -*-
 import random
 #Reordering the data in the dataset
-f=open('E:\\KEA_code\\data\\Semeval2010_244.json','rb')
+f=open('Data\\Semeval2010_244.json','rb')
 lines=f.readlines()
-w1=open('E:\\KEA_code\\data\\Semeval\\Semeval.range_244.json','w')
+w1=open('Data\\Semeval\\Semeval.range_244.json','w')
 num=[]
 con=[]
 for i in range(0,244):
