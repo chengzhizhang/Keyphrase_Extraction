58b7986b9cde19c9f843643a0dfe755164c29dbe
EEKE2020 <61223296+EEKE2020@users.noreply.github.com>
2021-06-15 20:33:05 +0800

    Create configs.py

---
+++ ML/configs.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+import os
+
+# Get the absolute path under the current working directory
+WORKING_PATH = os.path.dirname(os.getcwd())
+DATASET_PATH = os.path.join(WORKING_PATH, 'dataset')
