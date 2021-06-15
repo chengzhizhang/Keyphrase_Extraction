021bbb92e9f4905abb33da04e7998963d5bbf36c
chengzhizhang <61543993+chengzhizhang@users.noreply.github.com>
2021-06-15 20:08:23 +0800

    Add files via upload

---
+++ dl.bat
@@ -0,0 +1,41 @@
+:: Switch path
+cd DL
+
+:: Run on dataset SemEval-2010 -- without refs
+python main.py -dn SemEval-2010 -fd title abstract
+python main.py -dn SemEval-2010 -fd title abstract
+python main.py -dn SemEval-2010 -fd title abstract
+python main.py -dn SemEval-2010 -fd title abstract
+python main.py -dn SemEval-2010 -fd title abstract
+:: Run on dataset SemEval-2010 -- with refs
+python main.py -dn SemEval-2010 -fd title abstract references
+python main.py -dn SemEval-2010 -fd title abstract references
+python main.py -dn SemEval-2010 -fd title abstract references
+python main.py -dn SemEval-2010 -fd title abstract references
+python main.py -dn SemEval-2010 -fd title abstract references
+
+:: Run on dataset KP20k -- without refs
+python main.py -dn KP20k -fd title abstract
+python main.py -dn KP20k -fd title abstract
+python main.py -dn KP20k -fd title abstract
+python main.py -dn KP20k -fd title abstract
+python main.py -dn KP20k -fd title abstract
+:: Run on dataset KP20k -- with refs
+python main.py -dn KP20k -fd title abstract references
+python main.py -dn KP20k -fd title abstract references
+python main.py -dn KP20k -fd title abstract references
+python main.py -dn KP20k -fd title abstract references
+python main.py -dn KP20k -fd title abstract references
+
+:: Run on dataset LIS-2000 -- without refs
+python main.py -dn LIS-2000 -fd title abstract
+python main.py -dn LIS-2000 -fd title abstract
+python main.py -dn LIS-2000 -fd title abstract
+python main.py -dn LIS-2000 -fd title abstract
+python main.py -dn LIS-2000 -fd title abstract
+:: Run on dataset LIS-2000 -- with refs
+python main.py -dn LIS-2000 -fd title abstract references
+python main.py -dn LIS-2000 -fd title abstract references
+python main.py -dn LIS-2000 -fd title abstract references
+python main.py -dn LIS-2000 -fd title abstract references
+python main.py -dn LIS-2000 -fd title abstract references
