7d805c25d5187aa9552925174777ba4e2ee5fcf5
leizhao150 <84842397+leizhao150@users.noreply.github.com>
2021-06-17 09:11:36 +0800

    Update utils.py

--- DL/utils.py
+++ DL/utils.py
@@ -21,7 +21,7 @@ def parse_tags(texts, preds):
     return list(set(keywords))
 
 def cal_metrics(pred_keywords, target_keywords):
-    # 计算P, R
+    # Calculate P, R
     p_list, r_list = [], []
     for pred, target in zip(pred_keywords, target_keywords):
         num_pred_correct = len(set(pred) & set(target))
@@ -33,7 +33,7 @@ def cal_metrics(pred_keywords, target_keywords):
         r_list.append(r)
     P = np.average(p_list)
     R = np.average(r_list)
-    # 计算F1
+    # Calculate F1
     F1 = (2 * P * R) / (P + R) if (P + R) != 0 else 0
     return round(P * 100, 3), round(R * 100, 3), round(F1 * 100, 3)
 
@@ -54,23 +54,23 @@ def performance_metrics(pred_tags, test_info, is_add_ref):
         target_keywords, pred_keywords_in_tit_abs, pred_keywords = [], [], []
         for i in range(len(test_info)):
             key, item = test_info[i]
-            # 目标关键词
+            # Target keywords
             target = item[1]
-            # 源文本
+            # Source text
             texts = item[2]
-            # 出现在标题和摘要中的预测
+            # Preditions that appear in the title and abstract
             preds_in_tit_abs = pred_tags[item[0][0]: item[0][1]]
-            # 所有文档信息的预测
+            # Preditions of all document information
             preds_all = pred_tags[item[0][0]: item[0][0]+len(texts)]
-            # 在标题和摘要中预测的关键词
+            # Predicted keywords in the title and abstract
             keywords_in_tit_abs = parse_tags(texts, preds_in_tit_abs)
-            # 在所有文档信息中预测的关键词
+            # Predicted keywords in all document information
             keywords_all = parse_tags(texts, preds_all)
             target_keywords.append(target)
             pred_keywords_in_tit_abs.append(keywords_in_tit_abs)
             pred_keywords.append(keywords_all)
         metrics_all = cal_metrics(pred_keywords, target_keywords)
-        # 计算在标题和摘要中预测结果的性能
+        # Calculate the performance of predicting results in the title and abstract
         p_list, r_list = [], []
         for pred, pred_in_tit_abs, target in zip(pred_keywords_in_tit_abs, pred_keywords, target_keywords):
             num_pred_correct = len(set(pred) & set(target))
@@ -82,12 +82,12 @@ def performance_metrics(pred_tags, test_info, is_add_ref):
             r_list.append(r)
         P = np.average(p_list)
         R = np.average(r_list)
-        # 计算F1
+        # Calculate F1
         F1 = (2 * P * R) / (P + R) if (P + R) != 0 else 0
         metrics_in_tit_abs = (round(P * 100, 3), round(R * 100, 3), round(F1 * 100, 3))
         return metrics_all, metrics_in_tit_abs
 
-# 读取文件
+# Read file
 def read_json_datas(path):
     datas = []
     with open(path, "r", encoding="utf-8") as fp:
@@ -95,7 +95,7 @@ def read_json_datas(path):
             datas.append(json.loads(i))
     return datas
 
-# 保存数据
+# save data
 def save_datas(obj, path):
     datas = []
     for line in obj:
@@ -106,10 +106,10 @@ def save_datas(obj, path):
     with open(path, "w", encoding="utf-8") as fp:
         with tqdm(range(len(datas))) as pbar:
             for i in pbar:
-                pbar.set_description("第%s个文档保存完成" % (i + 1))
+                pbar.set_description("The %s document is saved" % (i + 1))
                 fp.write(json.dumps(datas[i])+"\n")
 
-# 从文件中读取单词
+# Read words from file
 def read_text(path):
     words = []
     with open(path, 'r', encoding='utf-8') as fp:
