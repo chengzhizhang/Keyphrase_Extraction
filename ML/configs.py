61a76b73d55c3023895bc2c73a5da667224417e5
chengzhizhang <61543993+chengzhizhang@users.noreply.github.com>
2021-09-29 19:17:38 +0800

    Delete ML directory

--- ML/configs.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-import os
-
-# Get the absolute path under the current working directory
-WORKING_PATH = os.path.dirname(os.getcwd())
-DATASET_PATH = os.path.join(WORKING_PATH, 'Dataset')
